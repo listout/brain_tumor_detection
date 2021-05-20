62c4d71dc77ac29aa4d110142c880386cba1224d
listout <listout@protonmail.com>
2021-05-20 11:10:49 +0530

    dataset has been given during model fitting

--- vgg_implementation.py
+++ vgg_implementation.py
@@ -82,17 +82,33 @@ vgg_model.summary()
 epochs = 120
 vgg_history = vgg_model.fit(
     training_set,
-    epochs=epochs
+    epochs=epochs,
+    validation_data=testing_set
 )
 
 # save model if need
 # model.save('/path/to/model/folder')
 
+# metrics
+# acc = vgg_history.history['accuracy']
+# val_acc = vgg_history.history['val_accuracy']
+# loss = vgg_history.history['loss']
+# val_loss = vgg_history.history['val_loss']
+
 # result plotting
-plt.plot(vgg_history.history['loss'], 'r', label='train')
-plt.plot(vgg_history.history['accuracy'], 'g', label='validation')
-plt.legend(['loss', 'accuracy'])
-plt.xlabel('epochs')
-plt.ylabel('measure')
-plt.tight_layout()
-plt.savefig('/content/drive/MyDrive/model/vgg_run1.png', bbox_inches='tight')
+# figs, ax = plt.subplots(2, sharex=True)
+# ax[0].plot(acc, label='Training Accuracy')
+# ax[0].plot(val_acc, label='Validation Accuracy')
+# ax[0].legend(loc='lower right')
+# ax[0].set_ylabel('Accuracy')
+# ax[0].set_title('Training and Validation Accuracy')
+
+# ax[1].plot(loss, label='Training Loss')
+# ax[1].plot(val_loss, label='Validation Loss')
+# ax[1].legend(loc='upper right')
+# ax[1].set_ylabel('Cross Entropy')
+# ax[1].set_title('Training and Validation Loss')
+# ax[1].set_xlabel('epoch')
+
+# plt.tight_layout()
+# plt.savefig('/content/drive/MyDrive/model/vgg_run1.png', bbox_inches='tight')
