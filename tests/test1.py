4a2ccab7e655030d37a40ad81f2df2eab63d92c1
listout <listout@protonmail.com>
2021-05-22 13:57:51 +0530

    correct ImageDataGenerator parameters

--- tests/test1.py
+++ tests/test1.py
@@ -23,15 +23,11 @@ model = tf.keras.models.load_model('model/vgg_model.h5')
 print(model.summary())
 
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
+    rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    brightness_range=(.3, 1.),
     horizontal_flip=True,
     vertical_flip=True,
-    fill_mode='nearest',
     preprocessing_function=preprocess_input,
     validation_split=0.2
 )
